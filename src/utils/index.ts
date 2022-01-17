9183c0a6f3595b45474080ae98a1bef703869b9a
Huboh <72440222+huboh@users.noreply.github.com>
2022-01-17 19:45:58 +0100

    👀 FEAT :  add `sendJson`, `startServer` utility functions

---
+++ src/utils/index.ts
@@ -0,0 +1,20 @@
+export { default as Errors } from './errors';
+export { default as DataBase } from './Database';
+export { default as handleError } from './errors/handleError';
+
+
+import { STATUS_CODES } from 'http';
+import { SendJsonProps } from '../types';
+import { Express, Response } from 'express';
+
+
+export function sendJson(response: Response, jsonData: SendJsonProps) {
+  const { statusCode, status, message, data, errors } = jsonData;
+
+  response.status(statusCode);
+  response.json({ status, data, errors, message: message ?? STATUS_CODES[statusCode] });
+}
+
+export function startServer(server: Express, port: number | string, callback?: () => void) {
+  server.listen(Number(port), callback);
+}
