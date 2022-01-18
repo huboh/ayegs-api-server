93bb59ab49186421c33bf9917685c4bd86530ce2
Huboh <72440222+huboh@users.noreply.github.com>
2022-01-18 15:52:49 +0100

    🥂 FEAT : add helper function to validate submitted user object

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,6 +4,8 @@ export { default as DataBase } from './Database';
 export { default as handleError } from './errors/handleError';
 
 
+import { Errors } from '.';
+import validator from 'validator';
 import { STATUS_CODES } from 'http';
 import { SendJsonProps, SubmittedUser } from '../types';
 import { Express, Response, RequestHandler } from 'express';
@@ -22,4 +24,23 @@ export function startServer(server: Express, port: number | string, callback?: (
 
 export function injectMiddlewares(server: Express, middlewares: RequestHandler[]) {
   server.use(...middlewares);
+}
+
+export function validateUser(user: SubmittedUser) {
+  const { email, password, firstName } = user;
+  const errors = [
+    validator.isEmail(email) ? true : 'invalid email address',
+    validator.isAlpha(firstName) ? true : 'invalid name, must be only contain letters',
+    validator.isStrongPassword(password, { minLength: 6 }) ? true : 'invalid password'
+  ].filter(
+    (e) => typeof e !== 'boolean'
+  );
+
+  if (errors.length) {
+    throw new Errors.ValidaionError('user validation error', errors);
+  }
+
+  return {
+    email, password, firstName
+  };
 }
