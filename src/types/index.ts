f8f99c0bda022362856b3c11ca538567c7c75b0f
Huboh <72440222+huboh@users.noreply.github.com>
2022-01-18 23:18:03 +0100

    DOC : add `user` interface type

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,3 +10,38 @@ export interface SendJsonProps {
   data?: unknown;
   errors?: unknown;
 }
+
+export interface SubmittedUser {
+  name: string;
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+}
+
+
+export interface User {
+  name: string;
+  lastName: string;
+  firstName: string;
+  isEmailVerified: boolean;
+  isPhoneVerified: boolean;
+  email: string;
+  password: string;
+  phone: string;
+  avatarUrl: string;
+  meta: {
+    location: {
+      city: string;
+      address: string;
+      country: string;
+      zipCode: number,
+    },
+    payment: {
+      expiry: string;
+      provider: string;
+      accountNo: string;
+      paymentType: string;
+    };
+  },
+}
