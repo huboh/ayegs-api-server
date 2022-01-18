942643b3428436d4a7eb9381d57187d7337c1080
Huboh <72440222+huboh@users.noreply.github.com>
2022-01-18 19:24:40 +0100

    👷‍♂️ FIX : return early if error is not our expected error

--- src/utils/errors/handleError.ts
+++ src/utils/errors/handleError.ts
@@ -7,25 +7,23 @@ const handleError = (error: unknown, request: Request, response: Response, next:
   let message = error instanceof Error ? error.message : undefined;
   let statusCode = 500;
 
-  if (error instanceof Errors.BaseError) {
-    errors = error.errors;
-
-    switch (error.name as keyof typeof Errors) {
-      case 'ValidaionError': statusCode = 406; break;
-      case 'ForbiddenError': statusCode = 403; break;
-      case 'NotAuthorized': statusCode = 401; break;
-      case 'AuthorizationError': statusCode = 401; break;
-      case 'InvalidPayloadError': statusCode = 400; break;
-      case 'InvalidResourceError': statusCode = 400; break;
-      case 'ServiceUnavailableError': statusCode = 503; break;
-      default: next(error); return;
-    }
-  } else {
+  if (!(error instanceof Errors.BaseError)) {
     return next(error);
   }
 
+  switch (error.name as keyof typeof Errors) {
+    case 'ValidaionError': statusCode = 406; break;
+    case 'ForbiddenError': statusCode = 403; break;
+    case 'NotAuthorized': statusCode = 401; break;
+    case 'AuthorizationError': statusCode = 401; break;
+    case 'InvalidPayloadError': statusCode = 400; break;
+    case 'InvalidResourceError': statusCode = 400; break;
+    case 'ServiceUnavailableError': statusCode = 503; break;
+    default: statusCode = 503; break;
+  }
+
   sendJson(response, {
-    errors,
+    errors: error.errors ?? errors,
     message,
     statusCode,
     status: 'error',
