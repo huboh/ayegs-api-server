3fc8ea8591927576e9440184af74779289d38df4
Huboh <72440222+huboh@users.noreply.github.com>
2022-01-19 21:24:42 +0100

    🤫 REFACTOR : export `SALT_ROUNDS` for interested code & refactor for beauty 🐱‍👤

--- src/utils/passwords/index.ts
+++ src/utils/passwords/index.ts
@@ -2,10 +2,16 @@ import bcrypt from 'bcrypt';
 
 const { SALT_ROUNDS } = process.env;
 
-export const hashPassword = async (password: string) => {
-  return bcrypt.hash(password, Number(SALT_ROUNDS) ?? 15);
-};
+const hashPassword = async (password: string) => (
+  bcrypt.hash(password, Number(SALT_ROUNDS) ?? 15)
+);
 
-export const comparePasswords = async (plainTextPassword: string, hashedPassword: string) => {
-  return bcrypt.compare(plainTextPassword, hashedPassword);
+const comparePasswords = async (plainTextPassword: string, hashedPassword: string) => (
+  bcrypt.compare(plainTextPassword, hashedPassword)
+);
+
+export {
+  comparePasswords,
+  hashPassword,
+  SALT_ROUNDS,
 };
