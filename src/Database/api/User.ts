b6acc08bca64b1be1ebe36f67ad360396c4e1cd6
Huboh <72440222+huboh@users.noreply.github.com>
2022-01-19 21:46:03 +0100

    🤙 FEAT : implement `getUser` util function & add error handling for `createUser` util function

--- src/Database/api/User.ts
+++ src/Database/api/User.ts
@@ -1,16 +1,26 @@
-import { User as UserType } from "../../types";
 import User from "../models/User";
+import { isValidObjectId } from 'mongoose';
 import { getUserFromObject } from "../utils";
+import { User as UserType, SubmittedUser } from "../../types";
+import { Errors, validateUser, passwords } from "../../utils";
 
 
 export default {
 
-  async getUser() {
-
+  async getUser(query: { email?: string; _id?: string; }): Promise<(UserType & { _id: string; }) | null> {
+    return (query._id && !isValidObjectId(query._id)) ? null : User.findOne({
+      ...query
+    });
   },
 
-  async createUser(user: Partial<UserType>) {
-    return User.create(getUserFromObject(user));
+  async createUser(userDetails: Partial<UserType>) {
+    if (await this.getUser({ email: userDetails.email })) throw new Errors.ForbiddenError(
+      'user already exists'
+    );
+
+    return User.create(
+      getUserFromObject(userDetails)
+    );
   },
 
   async updateUser() {
