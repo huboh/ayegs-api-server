e671a4962c07978bdc691c8d633265ffeae45f2b
Huboh <72440222+huboh@users.noreply.github.com>
2022-01-19 13:41:32 +0100

    🤙 register route handlers

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,25 @@ import * as routes from './routes';
 import middlewares from './middlewares';
 import { startServer, injectMiddlewares } from './utils';
 
+
+const database = new DataBase();
 const { PORT, DATABASE_HOST } = process.env;
 
 const app = express();
 const port = PORT ?? 4000;
 
-const database = new DataBase();
 
 injectMiddlewares(app, middlewares);
 
+
 app.use('/api', routes.mainRouter);
+app.use('/api/cart', routes.cartRouter);
+app.use('/api/account', routes.accountRouter);
 app.use('/api/products', routes.productRouter);
 
 app.use(routes.notFound);
 app.use(routes.serverError);
+
 
 database.connect({
   host: DATABASE_HOST!,
---
+++ src/routes/index.ts
@@ -0,0 +1,6 @@
+export { default as mainRouter } from "./main";
+export { default as cartRouter } from "./cart";
+export { default as accountRouter } from "./account";
+export { default as productRouter } from "./product";
+export { default as notFound } from "./handlers/notFound";
+export { default as serverError } from "./handlers/serverError";
