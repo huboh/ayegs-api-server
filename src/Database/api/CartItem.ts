ab3e1bbb72cecaacaad42600575177d04869f694
Huboh <72440222+huboh@users.noreply.github.com>
2022-01-21 21:53:47 +0100

    🥂 FEAT : add `CartItem` api function for working with cartitems collection

--- src/Database/api/CartItem.ts
+++ src/Database/api/CartItem.ts
@@ -1,29 +1,105 @@
+import User from "./User";
+import Product from "./Product";
 import CartItem from "../models/CartItem";
 
-import { Errors } from '../../utils';
-import { isValidObjectId } from 'mongoose';
-import { UserId, ProductId } from "../../types";
+import { Errors, verifyMongooseIdentifiers } from '../../utils';
+import { UserId, GetCartItemProp, AddCartItemProp, AddCartItemsProp, UpdateCartItemsProp, RemoveCartItemProp, CartItemExistsProp, } from "../../types";
 
 export default {
-  async getCartItems(userId: UserId): Promise<unknown[]> {
-    if (!isValidObjectId(userId)) throw new Errors.ValidationError(
-      'validation error', ['invalid idenitifier']
-    );
+  async cartItemsExists({ _id, productId, userId }: CartItemExistsProp) {
+    verifyMongooseIdentifiers(_id, productId, userId);
 
-    return CartItem.find(
-      { userId }
+    return CartItem.exists(
+      { $or: [{ userId, productId }, { _id }] }
     );
   },
 
-  async addCartItems(userId: UserId, productId: ProductId | ProductId[]) {
-    // TODO : verify product exists, verify user id, add the item to cart
+  async getCartItems(userId: UserId) {
+    verifyMongooseIdentifiers(userId);
 
-    if (!isValidObjectId(productId)) throw new Errors.ResourceNotFound(
-      'error getting product', ['invalid idenitifier']
+    const cartItems = await CartItem.find({ userId });
+    const total = cartItems.length;
+
+    return {
+      total,
+      cartItems,
+    };
+  },
+
+  async getCartItem({ _id, userId, productId }: GetCartItemProp) {
+    verifyMongooseIdentifiers(_id, userId, productId);
+
+    return CartItem.findOne(
+      { $or: [{ userId, productId }, { _id }] }
     );
   },
 
-  async addCartItem(userId: UserId, productIds: ProductId) {
+  async addCartItems({ userId, products }: AddCartItemsProp) {
+    if (!(products instanceof Array) || ((products instanceof Array) && !products.length)) {
+      throw new Errors.InvalidPayloadError('invalid request', ['payload must be an array containing cart item(s)']);
+    };
 
+    const results = await Promise.allSettled(products.map((product) => (
+      this.addCartItem({ userId, ...product })
+    )));
+
+    return results.map((result) => (
+      (result as any).value)
+    );
   },
+
+  async addCartItem({ userId, productId, quantity }: AddCartItemProp) {
+    let result = true;
+
+    try {
+      verifyMongooseIdentifiers(userId, productId);
+
+      quantity = isNaN(Number(quantity)) ? 1 : quantity;
+      const [productExists, userExists, itemInCart] = await Promise.all([
+        Product.productExists(productId), User.userExists({ _id: userId }), this.cartItemsExists({ userId, productId })
+      ]);
+
+      if (!productExists && !userExists) throw new Errors.ValidationError(
+        'validation error', ['error adding item to cart']
+      );
+
+      if (itemInCart) {
+        await this.updateQuantity({ quantity, userId, productId });
+      } else {
+        await CartItem.create({ quantity, userId, productId });
+      }
+
+    } catch (error) {
+      result = false;
+    } finally {
+      return result;
+    }
+  },
+
+  async removeCartItem({ userId, productId, _id }: RemoveCartItemProp) {
+    verifyMongooseIdentifiers(userId, productId, _id);
+
+    if (!(await this.cartItemsExists({ userId, productId, _id }))) throw new Errors.ForbiddenError(
+      'item does not exists in cart'
+    );
+
+    return (
+      await CartItem.deleteOne({ $or: [{ userId, productId }, { _id }] })
+    ).deletedCount === 1;
+  },
+
+  async updateQuantity({ _id, userId, productId, quantity }: UpdateCartItemsProp) {
+    verifyMongooseIdentifiers(_id, userId, productId);
+    quantity = isNaN(Number(quantity)) ? 1 : quantity;
+
+    const cartItem = await this.getCartItem({ userId, productId, _id });
+
+    if (!cartItem) throw new Errors.ResourceNotFound(
+      '', ['item does not exists in cart']
+    );
+
+    return ((cartItem.quantity + quantity) < 1) ? this.removeCartItem({ userId, productId, _id }) : CartItem.findOneAndUpdate(
+      { $or: [{ userId, productId }, { _id }] }, { $inc: { quantity } }, { new: true }
+    );
+  }
 };
