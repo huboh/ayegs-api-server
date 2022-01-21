9d1345f22ee289b4f1d6017921b512627e6d6e30
Huboh <72440222+huboh@users.noreply.github.com>
2022-01-21 15:18:40 +0100

    FEAT : add route for adding cart items

--- src/routes/cart/index.ts
+++ src/routes/cart/index.ts
@@ -1,19 +1,39 @@
 import { Router } from 'express';
-import { sendJson, handleError, Errors } from '../../utils';
+import DataBase from '../../Database';
+import * as auth from '../../middlewares/auth';
+import { sendJson, handleError, injectMiddlewares } from '../../utils';
 
+const database = new DataBase();
 const cartRouter = Router({ caseSensitive: false });
 
-cartRouter.get('/', async (request, response, next) => {
+injectMiddlewares(cartRouter, [auth.requiresAuthMiddleware]);
+
+cartRouter.get('/items', async (request, response, next) => {
   try {
-    const statusCode = 200;
+    const { _id: userId } = response.locals.user;
+    const cartItems = await database.CartItem.getCartItems(userId);
 
     sendJson(response, {
-      statusCode,
+      statusCode: 200,
       status: 'success',
-      data: {
-        count: 10,
-        cartItems: []
-      }
+      data: { count: cartItems.length, cartItems }
+    });
+
+  } catch (error) {
+    handleError(error, request, response, next);
+  }
+});
+
+cartRouter.post('/add', async (request, response, next) => {
+  try {
+    const { productIds } = request.body;
+    const { _id: userId } = response.locals.user;
+    const cartItems = await database.CartItem.addCartItems(userId, productIds);
+
+    sendJson(response, {
+      statusCode: 200,
+      status: 'success',
+      // data: { count: cartItems.length, cartItems }
     });
 
   } catch (error) {
