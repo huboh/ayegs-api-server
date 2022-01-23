801a1ce6b24dba4d8b8380aae4b2f1b483865e78
Huboh <72440222+huboh@users.noreply.github.com>
2022-01-23 15:20:57 +0100

    FEAT : create types for models & nominal kindof types for id's

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,20 @@
 import { ObjectId } from 'mongoose';
 
-export type UserId = Object | string;
-export type ProductId = Object | string;
-export type CartItemId = Object | string;
+export type UserId = ObjectId | string;
+export type OrderId = ObjectId | string;
+export type ProductId = ObjectId | string;
+export type CartItemId = ObjectId | string;
+export type CategoryId = ObjectId | string;
 
+export type OrderStatus = 'processing' | 'shipped' | 'in transit' | 'delivered';;
 export type ResponseStatusText = 'success' | 'error';
+
+export interface MongoDBProps {
+  __v: number;
+  _id: string | ObjectId;
+  createdAt: Date | string;
+  updatedAt: Date | string;
+}
 
 export interface SendJsonProps {
   status: ResponseStatusText;
@@ -23,37 +33,94 @@ export interface SubmittedUser {
 }
 
 export interface User {
-  name: string | null;
-  lastName: string | null;
-  firstName: string | null;
+  name: string;
+  email: string;
+  phone: string;
+  password: string;
+  avatarUrl: string;
+  lastName: string;
+  firstName: string;
   isEmailVerified: boolean;
   isPhoneVerified: boolean;
-  email: string | null;
-  phone: string | null;
-  avatarUrl: string | null;
   meta: {
     location: {
-      city: string | null;
-      address: string | null;
-      country: string | null;
-      zipCode: number | null,
+      city: string;
+      address: string;
+      country: string;
+      zipCode: number,
     },
     payment: {
-      expiry: string | null;
-      provider: string | null;
-      accountNo: string | null;
-      paymentType: string | null;
+      expiry: string;
+      provider: string;
+      accountNo: string;
+      paymentType: string;
     };
   },
 }
 
-export interface UserFromDB extends User {
-  _id: UserId;
-  __v: number;
-  password: string;
-  createdAt: Date | string;
-  updatedAt: Date | string;
+export interface Product {
+  title: string;
+  mainImageUrl: string,
+  meta: {
+    categoriesId: CategoryId[];
+    imagesUrl: string;
+    stock: number;
+    sku: string;
+    price: {
+      currency: string;
+      current: string;
+      previous: string;
+    },
+    desc: {
+      long: string;
+      short: string;
+    },
+    reviews: {
+      average: number;
+      count: number;
+    },
+    discount: {
+      percent: number;
+      active: boolean;
+    };
+  };
 }
+
+export interface Order {
+  userId: UserId;
+  status: OrderStatus;
+  productId: ProductId;
+  trackingId: string;
+  meta: {
+    quantity: number;
+    shippingDetails: {
+      city: string;
+      zipCode: number;
+      country: string;
+      address: string;
+    },
+  };
+}
+
+export interface CartItem {
+  quantity: number;
+  userId: UserId;
+  productId: ProductId;
+}
+
+export interface Category {
+  name: string;
+  desc: {
+    long: string;
+    short: string;
+  };
+}
+
+export interface UserModel extends MongoDBProps, User { }
+export interface OrderModel extends MongoDBProps, Order { }
+export interface ProductModel extends MongoDBProps, Product { }
+export interface CartItemModel extends MongoDBProps, CartItem { }
+export interface CategoryModel extends MongoDBProps, Category { }
 
 export interface GetProductProps {
   _id?: UserId;
@@ -105,4 +172,30 @@ export interface CartItemExistsProp {
   userId: UserId;
   _id?: CartItemId;
   productId?: ProductId;
+}
+
+export interface CreateOrderProp {
+  orderDetails: Order;
+}
+
+export interface GetOrderProp {
+  userId: UserId;
+  orderId: OrderId;
+}
+
+export interface UpdateOrderProp {
+  userId: UserId;
+  orderId: OrderId;
+  newOrderDetails: Partial<Order>;
+}
+
+export interface UpdateOrderStatusProp {
+  userId: UserId;
+  orderId: OrderId;
+  orderStatus: Order['status'];
+}
+
+export interface RemoveOrderProp {
+  userId: UserId;
+  orderId: OrderId;
 }
